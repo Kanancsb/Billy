42a44c36794140298ddfef8e02f1c0d7f1150c79
Kanan Cassio dos Santos Bernardi <kanancsb@gmail.com>
2023-02-02 10:28:25 -0300

    Scripts
    This is the scripts that i used until now

---
+++ Scripts/DoorOpen.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorOpen : MonoBehaviour
+{
+
+    public float TheDistance;
+    public GameObject ActionDisplay;
+    public GameObject ActionText;
+    public GameObject TheDoor;
+    public AudioSource CreakSound;
+
+    // Update is called once per frame
+    void Update(){
+
+        TheDistance = PlayerCasting.DistanceFromTarget;
+
+    }
+
+    void OnMouseOver(){
+
+        if (TheDistance <= 3){
+            ActionDisplay.SetActive(true);
+            ActionText.SetActive(true);
+        }
+
+        if (Input.GetButtonDown("Action")){
+            if (TheDistance <= 3){
+                this.GetComponent<BoxCollider>().enabled = false;
+                ActionDisplay.SetActive(false);
+                ActionText.SetActive(false);
+                TheDoor.GetComponent<Animation>().Play("DoorAnim");
+                CreakSound.Play();
+            }
+        } 
+    }
+
+    void OnMouseExit(){
+
+        ActionDisplay.SetActive(false);
+        ActionText.SetActive(false);
+    }
+}
